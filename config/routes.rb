3f6cc92c42a18eab0a6d6b204094368854f95c3c
Zoe Rose zoealexandrarose@outlook.com <zoealexandrarose@outlook.com>
2022-11-15 15:16:43 +0100

    did all CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new'
+  get '/articles/:id', to: 'articles#show', as: 'article'
+  post '/articles', to: 'articles#create'
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  patch 'articles/:id', to: 'articles#update'
+  delete "articles/:id", to: "articles#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+10.times do
+  article = Article.new(
+    title: Faker::Movie.title,
+    content: Faker::Movie.quote
+    )
+  article.save!
+end
